d7567a579a80da45e6a7f8602b39cb9b6f9c9560
catherine <vuthi.esther@gmail.com>
2022-06-09 06:09:24 +0300

    commit tree 078f73b551cdbb1f64e350f33c055d9ef96c4a2e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+    # Adds add between element when array has 2 elements
+     return  array.join(" and ") if array.size < 3
+
+     #inserts comma and finl and when given 3 elements
+     array[-1] = "and #{array[-1]}"
+
+     #separates array with a comma
+     array.join(", ")
+  
+    
 end
